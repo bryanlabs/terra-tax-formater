ecdfa2aa707f52cbc7d04404370363390dc916d0
Peter Harral <pjharral@gmail.com>
2021-10-31 22:26:01 -0400

    Find list of txs from FCD that were not found in input file csv

--- terra_tax_formatter/terra_tax_formatter.py
+++ terra_tax_formatter/terra_tax_formatter.py
@@ -4,8 +4,9 @@ from .files import *
 from .arg_parsing import parse_args, validate_args
 from .exceptions import ConverterCaughtError
 from .terra_fcd import get_all_tx_for_address, make_txhash_map
+from .terra_lcd import get_tx_info_by_txhash
 from .stake_tax_csv_parsing import get_unique_tx_hashes
-import json, csv
+import csv
 
 def main():
     args = parse_args()
@@ -14,20 +15,23 @@ def main():
     input_file = args["input_file"]
 
     try:
-        tx_map = make_txhash_map(get_all_tx_for_address(terra_address))
+        all_fcd_txs = get_all_tx_for_address(terra_address)
+        tx_map = make_txhash_map(all_fcd_txs)
         txhashes = tx_map.keys()
 
+        not_found = []
+        stake_tax_txs = []
+        unique_txs = []
         with open_file(input_file) as infile:
             reader = list(csv.DictReader(infile))
-            unique_txs = get_unique_tx_hashes(reader)
-            unique_txs = dict.fromkeys(unique_txs, True)
+            stake_tax_txs = get_unique_tx_hashes(reader)
+            unique_txs = dict.fromkeys(stake_tax_txs, True)
 
-            not_found = []
-            for txhash in txhashes:
-                try:
-                    unique_txs[txhash]
-                except KeyError:
-                    not_found.append(txhash)
+        for txhash in txhashes:
+            try:
+                unique_txs[txhash]
+            except KeyError:
+                not_found.append(tx_map[txhash])
 
     except ConverterCaughtError as error:
         print(error.message)
