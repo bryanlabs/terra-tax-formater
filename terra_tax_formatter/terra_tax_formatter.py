a3b2f4f8cd11e991863d6f76a893e2a028d97196
Peter Harral <pjharral@gmail.com>
2021-11-01 23:03:02 -0400

    Parse missing txs into csv output file (WIP), leave original input file intact

--- terra_tax_formatter/terra_tax_formatter.py
+++ terra_tax_formatter/terra_tax_formatter.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
 
+from os import write
 from .files import *
 from .arg_parsing import parse_args, validate_args
 from .exceptions import ConverterCaughtError
 from .terra_fcd import get_all_tx_for_address, make_txhash_map
 from .terra_lcd import get_tx_info_by_txhash
 from .stake_tax_csv_parsing import get_unique_tx_hashes
-import csv
+import csv, json, base64
 
 def main():
     args = parse_args()
     validate_args(args)
     terra_address = args["terra_address"]
     input_file = args["input_file"]
+    output_file = args["output_file"]
 
     try:
         all_fcd_txs = get_all_tx_for_address(terra_address)
@@ -32,6 +34,24 @@ def main():
                 unique_txs[txhash]
             except KeyError:
                 not_found.append(tx_map[txhash])
+
+        headers = ["timestamp", "txhash", "type", "value"]
+        new_rows = [headers]
+        for tx in not_found:
+            tx_info = tx["tx"]
+            messages = tx_info["value"]["msg"]
+            for message in messages:
+                message_type = message["type"]
+                message_value = message["value"]
+
+                #convert legacy columbus-4 execute_msg from base64 encoded json to regular json
+                if "execute_msg" in message_value.keys() and isinstance(message_value["execute_msg"], str):
+                    message_value["execute_msg"] = json.loads(base64.b64decode(message_value["execute_msg"]))
+            
+                message_value = json.dumps(message_value)
+                new_rows.append([tx["timestamp"], tx["txhash"], message_type, message_value])
+
+        write_csv(output_file, new_rows)
 
     except ConverterCaughtError as error:
         print(error.message)
